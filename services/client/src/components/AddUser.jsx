d0c9d0fe66f55ced3fa4dd776916862661d427ce
abdullah <abdullahabdelrhim@gmail.com>
2019-08-28 23:34:00 +0200

    add react frontend

---
+++ services/client/src/components/AddUser.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+ 
+const AddUser = (props) =>{
+    return (
+    <form onSubmit={(e) => props.addUser(e)}>
+      <div className="field">
+        <input
+          name="username"
+          className="input is-large"
+          type="text"
+          placeholder="Enter a username"
+          required
+          value={props.username}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className="field">
+        <input
+          name="email"
+          className="input is-large"
+          type="email"
+          placeholder="Enter an email address"
+          required
+          value={props.email}
+          onChange={props.handleChange}
+        />
+      </div>
+      <input
+        name="submit"
+        type="submit"
+        className="button is-primary is-large is-fullwidth"
+        value="Submit"
+      />
+    </form>
+    )
+};
+
+export default AddUser;
